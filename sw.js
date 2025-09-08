d167aea0969f212d0978a42516c5b4c7932c7c6e
uji <94710096+ujicos@users.noreply.github.com>
2025-09-08 14:46:45 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'budsjett-cache-v1.083'; // øk versjon for å invalidere gammel cache
+const CACHE_NAME = 'budsjett-cache-v1.09'; // øk versjon for å invalidere gammel cache
 const ASSETS_TO_CACHE = [
   '/',
   '/index.html',
